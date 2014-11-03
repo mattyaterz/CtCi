9d91076032fc006f87c7ecc0c19ddbc38e85f60b
Yates, Matt <matt.yates@tekcomms.com>
2014-11-02 22:13:20 -0600

    Chapter 1, Exercise 5

---
+++ src/main/java/com/github/yaterz/ctci/ci/ChapterOneExerciseFiveSolution.java
@@ -0,0 +1,48 @@
+package com.github.yaterz.ctci.ci;
+
+/**
+ * Implement a method to perform basic string compression using the counts of repeated characters.
+ * E.g. - aabcccccaaa would become a2b1c5a3
+ * If the 'compressed' string would not be smaller, return the original
+ * Assume only a-z and A-Z strings
+ *
+ * @author mattyaterz
+ * @since 11/2/14
+ */
+public class ChapterOneExerciseFiveSolution {
+    public static char[] getRepeatCompressedString( char[] str ) {
+        if ( str == null ) {
+            return null;
+        }
+
+        int compressedIndex = 0;
+        int duplicateCount = 1;
+        char[] compressedStr = new char[ str.length ];
+
+        for ( int i = 1; i <= str.length; i++ ) {
+            if ( i < str.length && str[ i - 1 ] == str[ i ] ) {
+                duplicateCount++;
+                continue;
+            }
+
+            String duplicateCountStr = Integer.toString( duplicateCount, 10 );
+
+            if ( compressedIndex < compressedStr.length - duplicateCountStr.length() ) {
+                compressedStr[ compressedIndex++ ] = str[ i - 1 ];
+
+                for ( char digit : duplicateCountStr.toCharArray() ) {
+                    compressedStr[ compressedIndex++ ] = digit;
+                }
+
+                duplicateCount = 1;
+            }
+            else {
+                return str;
+            }
+        }
+
+        char[] trimmedCompressed = new char[ compressedIndex ];
+        System.arraycopy( compressedStr, 0, trimmedCompressed, 0, compressedIndex );
+        return trimmedCompressed;
+    }
+}
---
+++ src/test/java/com/github/yaterz/ctci/ci/ChapterOneExerciseFiveTest.java
@@ -0,0 +1,37 @@
+package com.github.yaterz.ctci.ci;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * @author mattyaterz
+ * @since 11/2/14
+ */
+@RunWith( Parameterized.class ) public class ChapterOneExerciseFiveTest {
+    @Parameters public static Collection<String[]> testData() {
+        return Arrays.asList(
+                new String[] { "aabcccccaaa", "a2b1c5a3" },
+                new String[] { "ABCCDDDEEEEEFFFFFFFFGGGGGGGGGGGGGHHHHHHHHHHHHHHHHHHHHH", "A1B1C2D3E5F8G13H21" },
+                new String[] { "TheQuickBrownFoxJumpsOverTheLazyDog", "TheQuickBrownFoxJumpsOverTheLazyDog" },
+                new String[] { "", "" },
+                new String[] { null, null }
+        );
+    }
+
+    public ChapterOneExerciseFiveTest( String input, String output ) {
+        this.input = input != null ? input.toCharArray() : null;
+        this.output = output != null ? output.toCharArray() : null;
+    }
+
+    @Test public void testSimpleCompression() {
+        Assert.assertArrayEquals( output, ChapterOneExerciseFiveSolution.getRepeatCompressedString( input ) );
+    }
+
+    char[] input;
+    char[] output;
+}
